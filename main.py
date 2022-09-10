ec48afe46f7063beb364a9c1b5f335cfb71c32be
S.Alireza Ezaz <sar.ezaz@yahoo.com>
2022-09-10 12:51:27 +0430

    Filtered dependency candidates.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import numpy as np
 import time
 
 
-# Convert timestamp to time.
 def ts_to_time(ts):
+    # Convert timestamp to time.
     time_array = time.localtime(ts)
     result = time.strftime("%Y-%m-%d %H:%M", time_array)
     return result
@@ -85,18 +85,36 @@ def create_status(ds):
                         'to_duration_sum',
                         'from_err_num_sum',
                         'to_err_num_sum'], inplace=True)
-    # print(tmpdf.reset_index())
-    # with open('service-status.csv', 'w') as f:
-    #     tmpdf.to_csv(f)
     return tmpdf, unique_services
 
 
-# Section 1: Candidate selection
+# Section 1-1: Candidate selection
 # Given the raw traces, we first
 # generate a set of candidate service pairs (P, C) where service
 # P directly invokes service C and P and C are different services.
+print('Loading dataset...')
 dataset = load_dataset('status_1min_20210411.csv.xz')
+print('Dataset loaded.\n')
+
+print('Creating candidate pairs...')
 dependency_candidates = create_candidate_pairs(dataset)
+child_set = set(map(lambda x: x['c'], dependency_candidates))
+parent_set = set(map(lambda x: x['p'], dependency_candidates))
+print('Number of dependency candidates: {}'.format(len(dependency_candidates)))
+print('Number of child services: {}'.format(len(child_set)))
+print('Number of parent services: {}'.format(len(parent_set)))
+print('Number of services in both child and parent sets: {}'.format(len(child_set & parent_set)))
+print('Number of services in child set but not in parent set: {}'.format(len(child_set - parent_set)))
+print('Number of services in parent set but not in child set: {}\n'.format(len(parent_set - child_set)))
+# with open('dependency-candidates.csv', 'w') as f:
+#     f.write('parent_id,child_id,cnt\n')
+#     for item in dependency_candidates:
+#         f.write("%s,%s,%s\n" % (item['p'], item['c'], item['cnt']))
+
+# Section 1-2: Candidate Filtering
+# Only keep candidate calls whose parents appear as others' children and sort them by the number of calls.
+filtered_candidates = sorted(filter(lambda x: x['p'] in child_set, dependency_candidates), key=lambda x: x['cnt'],
+                             reverse=True)
 
 # Section 2-1: Status generation
 # The status of one service is composed
@@ -104,7 +122,11 @@ dependency_candidates = create_candidate_pairs(dataset)
 # duration of invocations, error of invocations. Each aspect of
 # the service’s status contains one or more Key Performance
 # Indicators (KPIs),
+print('Generating service status...')
 modified_dataset, unique_services = create_status(dataset)
+print('Number of unique services: {}\n'.format(len(unique_services)))
+# with open('service-status.csv', 'w') as f:
+#     modified_dataset.to_csv(f)
 
 # Section 2-2: Extracting KPIs(Key Performance Indicators)
 #  If a service is not invoked in a particular bin (i.e.,
@@ -112,5 +134,7 @@ modified_dataset, unique_services = create_status(dataset)
 # the end, we get the KPIs of every service M at every period
 # t. Ordering the bins by t, we get three time series of KPIs for each cloud service
 KPIs = list(modified_dataset.columns)
-
-print(*KPIs, sep='\n')
+# with open('KPIs.csv', 'w') as f:
+#     f.write('KPIs\n')
+#     for item in KPIs:
+#         f.write("%s\n" % item)
